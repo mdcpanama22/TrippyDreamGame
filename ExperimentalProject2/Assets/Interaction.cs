4d966eee295e9da1028dcd2c5cecec0fabf9e485
Carlson <carlsm4@win.rpi.edu>
2018-10-10 14:22:07 -0400

    added a cursor prefab, as well as the basic raycast interaction.

---
+++ ExperimentalProject2/Assets/Interaction.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interaction : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit hit;
+			Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.DrawRay(ray.origin, ray.direction * 500, Color.red);
+            }
+        }
+    }
+}
