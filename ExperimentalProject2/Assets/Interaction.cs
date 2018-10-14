9716fd55170749fb47463d5883cb4cb5fd8759ce
Matthew Carlson <mdcpanama22@gmail.com>
2018-10-14 18:43:26 -0400

    All changes will be reflected on Matthew_FinalScene_Testing. Renamed FPSController to RealFPSController and saved as a prefab. Renamed Terrain to island, did not save as a prefab. Renamed Dialogue to Smoker_Dialogue, and moved the textbox location, it was cutting out the last bit, and I centered the text more. I added ORBIT, which is a orbit of 7 blocks, that when clicked open to reveal a sphere that lets out the colors of the rainbow. oh and every 3 seconds it changes speed.

--- ExperimentalProject2/Assets/Interaction.cs
+++ ExperimentalProject2/Assets/Interaction.cs
@@ -40,15 +40,12 @@ public class Interaction : MonoBehaviour {
                 //CHECK FOR WHICH GAMEOBJECT YOU COLLIDED WITH TO THEN START INTERACTION
 				if(hit.transform.tag == "Object_I"){
                     hit.transform.GetComponent<_IndividualInteraction>().BoxTestInteraction();
-<<<<<<< HEAD
 				}else if(hit.transform.tag == "Object_IE") {
                     hit.transform.GetComponent<_IndividualInteraction>().BoxDisplacement();
-=======
 				}
                 else
                 {
                     hit.transform.gameObject.SendMessage("Trigger");
->>>>>>> cc334ab37d7194efeca42957e5087c4205d2f769
                 }
             }
         }
--- ExperimentalProject2/Assets/Movement_Around.cs
+++ ExperimentalProject2/Assets/Movement_Around.cs
@@ -10,13 +10,18 @@ public class Movement_Around : MonoBehaviour {
     private void Start()
     {
         transform.GetChild(0).GetComponent<ParticleSystem>().Stop();
+        InvokeRepeating("ChangeSpeed", 1.0f, 5.0f);
+    }
+    void ChangeSpeed()
+    {
+        speed = Random.Range(-120f, 120);
+        speed2 = Random.Range(-80f, 80);
     }
     // Update is called once per frame
     void Update () {
         if (GetComponent<_IndividualInteraction>()._BoxDisplaced){
-            Transform player = GameObject.Find("CENTER").transform;
             Transform current = transform;
-            transform.RotateAround(player.position, player.up, speed * Time.deltaTime);
+            transform.RotateAround(new Vector3(0f,0f,0f), Vector3.up, speed * Time.deltaTime);
             transform.RotateAround(current.position, current.up, speed2 * Time.deltaTime);
         }
 	}
