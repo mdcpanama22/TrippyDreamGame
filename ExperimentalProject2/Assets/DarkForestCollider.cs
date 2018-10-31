b01240c300ededd59a0454121f131bb63278d80a
Matthew Carlson <mdcpanama22@gmail.com>
2018-10-31 05:50:39 -0400

    Added skybox changer + Underwater Section

---
+++ ExperimentalProject2/Assets/DarkForestCollider.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DarkForestCollider : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+       
+        Debug.Log(other.transform.GetChild(0).gameObject.name);
+        if (other.transform.GetChild(0).gameObject.tag == "MainCamera")
+        {
+            SkyBoxShifter SH = other.transform.GetChild(0).gameObject.GetComponent<SkyBoxShifter>();
+            SH.DayTypeB = !SH.DayTypeB;
+
+            if (SH.DayTypeB)
+                SH.DayType = SkyBoxShifter.TimeOfDay.DarkForest;
+            else
+                SH.DayType = SkyBoxShifter.TimeOfDay.Main;
+        }
+    }
+}
